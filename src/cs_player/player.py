3d02fd09d5a7adc517c4678296b54701f81981b4
Jordan Marry <jmarry@terpmail.umd.edu>
2023-04-12 01:57:10 -0400

    retrieved all player stats

--- src/cs_player/player.py
+++ src/cs_player/player.py
@@ -13,16 +13,55 @@ class player():
         self.soup = BeautifulSoup(html_page, "lxml")
 
     def get_player_info(self):  
-        return self.soup.findAll('p')[0]
+        return self.soup.findAll('p')[0].get_text()
+
+    def get_player_infobox(self):
+        pass
 
     def get_links(self):
-        pass
+        div = self.soup.find("div", {"class": "infobox-center infobox-icons"})
+        links = div.find_all('a')
+        links_text = []
+
+        for link in links:
+            links_text.append(link['href'])
+        return links_text
     
     def get_history(self):
-        pass
+        hist = self.soup.find_all("div", {"class": "infobox-center"})
+        hist_div = hist[1].find_all("div")
+        team_history = []
+        for i in range(0, len(hist_div), 4):
+            team_history.append(hist_div[i].get_text())
+
+        return team_history
     
     def get_achieve(self):
-        pass
+        table = self.soup.find("table", {"class" : "wikitable wikitable-striped sortable"}).find("tbody").find_all("tr")
+        achieve = []
+        for i in range(1, len(table)-1):
+            data = table[i].find_all("td")
+            date = data[0].get_text()
+            place = data[1].get_text()
+            tourney_type = data[3].get_text()
+            tourney_name = data[6].get_text()
+            team_name = data[7].find("span")['data-highlightingclass']
+            score = data[8].get_text()
+            score = score.replace(u'\xa0', u' ')
+            try:
+                opposing = data[9].find("span")['data-highlightingclass']
+            except:
+                opposing = data[9].find("abbr")['title']
+
+            prize = data[10].get_text()
+    
+            achieve.append([date, place, tourney_type, tourney_name, team_name, score, opposing, prize])
+
+        return achieve
+
+
+
+
 
     def get_player_name(self):
         return self.player_name
--- src/cs_player/test.py
+++ src/cs_player/test.py
@@ -1,9 +1,23 @@
 from player import player
 
-cs_player = player('twistzz')
+cs_player = player('s1mple')
 
-print(cs_player.get_player_name())
+print("PLAYER NAME: " + cs_player.get_player_name() +"\n")
 
-print(cs_player.get_url())
+print("LIQUIPEDIA URL: " + cs_player.get_url() + "\n")
 
+print("PLAYER INFO: \n")
 print(cs_player.get_player_info())
+print("\n")
+
+print("PLAYER LINKS: \n")
+print(cs_player.get_links())
+print("\n")
+
+print("PLAYER HISTORY: \n")
+print(cs_player.get_history())
+print("\n")
+
+print("PLAYER ACHIEVEMENTS: \n")
+print(cs_player.get_achieve())
+print("\n")
